7c99853477590c89aa546fc6378a14261b4a14f1
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-08-16 16:16:38 +0200

    start button updates turns

--- player.js
+++ player.js
@@ -19,7 +19,21 @@ function playingPlayers (){
 
 function startGame (){
     playingPlayers()
+    turnsStart()
+    renderPlayer()
     currentThrow()
+}
+
+function currentPlayer(){
+    let players = [selectElements(".playing")]
+    let currentPlayerDiv = players.sort((a,b) => players[a].dataset.turn - players[b].dataset.turn)[0]
+    let currentPlayerName = currentPlayerDiv[0].children[0].children[0].value
+    return currentPlayerName
+}
+
+function renderPlayer(){
+    let nameSign = selectElement("#playerName")
+    nameSign.textContent = currentPlayer()
 }
 
 function currentThrow(){
@@ -27,4 +41,11 @@ function currentThrow(){
     let rollCounter = selectElement("#nmbRolls")
     rollCounter.innerText = `${player.dataset.throw}/3`
 
+}
+
+function turnsStart (){
+    let players = selectElements(".playing")
+    for (let player of players){
+        player.dataset.turn = 0
+    }
 }
